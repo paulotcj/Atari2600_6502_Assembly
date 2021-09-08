7904e181abd6331c7d153e51b42a31f31ebea34a
Paulo <none@none.com>
2021-09-08 18:32:55 -0400

    .

--- cleanmem.asm
+++ cleanmem.asm
@@ -1,24 +1,25 @@
     processor 6502
 
     seg code
-    org $F00            ;Define the code origin at $F00
+    org $F000       ; Define the code origin at $F000
 
 Start:
-    sei                 ;Disable interrupts
-    cld                 ;Disable the BCD decimal math mode
-    ldx #$FF            ;Load the X register with #$FF
-    txs                 ;Transfer the X register to the (S)tack pointer
+    sei             	; Disable interrupts
+    cld                 ; Disable the BCD decimal math mode
+    ldx #$FF        	; Loads the X register with #$FF
+    txs                 ; Transfer the X register to the (S)tack pointer
     
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Clear the Page Zero Region ($00 to $FF)
 ; Meaning the entire RAM and also the entire TIA registers
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     lda #0              ; A = 0
-    ldx #$FF            ; X = #$FF
+    LDX #$FF            ; X = #$FF
+    sta $FF             ; Make sure $FF is zeroed before the loop starts
     
 MemLoop:
-    sta $0,X            ; Store the value of A inside memory address $0 + X
     dex                 ; X--
+    sta $0,X            ; Store the value of A inside memory address $0 + X
     bne MemLoop         ; Loop until X is equal to zero (z-flag is set)
     
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -26,7 +27,5 @@ MemLoop:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
     org $FFFC
-    .word Start         ;Start vector at $FFFC (where the program starts)
-    .word Start         ;Interrupt vector at $FFFE (unused in the VCS)
-
-    
+    .word Start     	; Reset vector at $FFFC (where the program starts)
+    .word Start     	; Interrupt vector at $FFFE (unused in the VCS)
